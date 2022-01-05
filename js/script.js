491bb792411ec95623ce81d01a46b97580737845
dvenomb98 <91594441+dvenomb98@users.noreply.github.com>
2022-01-05 15:45:28 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,121 @@
+$(document).ready(function(){
+
+
+    var textbox = $('.text-box');
+    var circle =$('.circle');
+    var circle2 = $('.circle2');
+   
+
+//CHANGING COMPLETE STATE ON CLICK //
+ $('.elements').on('click','.text-box', function () {
+        $(this).toggleClass("default done");
+        $(this).find("p:first").toggleClass('circle circle2');
+        $(this).find('img').toggleClass('img-inactive img-active');
+        $(this).find('p:last').toggleClass ('text text2');
+
+
+        // recount
+        var n = $( ".text-box.default").length;
+        $( ".items-left" ).text( "" + n + " items left")
+    });
+    //CREATING AN ELEMENT
+    $(':input').on('keypress',function(e) {
+        if(e.which == 13 && $('#text').val().length != 0) {
+               
+                var data = $('#text').val();
+
+                $('.elements').append
+                ('<div class="text-box default"><p class="circle"><img class="img-inactive" src="img/icon-check.svg"></p><p class="text">' + data + '</p></div>');
+
+                // recount
+                var n = $( ".text-box.default").length;
+                $( ".items-left" ).text( "" + n + " items left");
+
+                //display all
+                $('.text-box.default').show()
+                $('.text-box.done').show()
+       
+        //Clear after usage
+        $('#text').val("") 
+     }});
+
+     // COMPLETED
+
+     var completed = $('.completed')
+
+     completed.on('click', function(){
+
+        $('.text-box.done').show()
+        $('.text-box.default').hide()
+    // recount
+        var n = $( ".text-box.done").length;
+        $( ".items-left" ).text( "" + n + " items left")
+
+     })
+
+     //ACTIVE
+
+     var active = $('.active');
+
+     active.on('click', function(){
+
+        $('.text-box.default').show()
+        $('.text-box.done').hide()
+    // recount
+        var n = $( ".text-box.default").length;
+        $( ".items-left" ).text( "" + n + " items left")
+     })
+
+     // ALL
+
+     var all = $('.all');
+
+     all.on('click', function(){
+
+        $('.text-box.default').show()
+        $('.text-box.done').show()
+    // recount
+        var n = $( ".text-box.default").length;
+        $( ".items-left" ).text( "" + n + " items left")
+     })
+      
+
+     // CLEAR
+
+     var clear = $('.clear')
+        clear.on('click', function() {
+        $('.text-box.done').remove()
+     //recount
+     
+        var n = $( ".text-box.default" ).length;
+         $( ".items-left" ).text( "" + n + " items left");
+     })
+
+     // SWITCHER
+
+     var plusmin = 1;
+
+     $('.switcher').click(function(){
+       plusmin *= -1;
+      var stl = plusmin>0 ? 'css/style.css' : 'css/style2.css';
+
+      $('#stl[rel=stylesheet]').attr('href', stl);
+     
+   });
+
+   $('.switcher2').click(function(){
+      plusmin *= -1;
+     var stl = plusmin>0 ? 'css/style.css' : 'css/style2.css';
+
+     $('#stl[rel=stylesheet]').attr('href', stl);
+    
+  });
+
+     
+
+    
+
+
+
+
+})
